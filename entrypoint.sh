89e64cbb34df085367a238aeed12dd6946c58c4b
hghbte <154860440+hghbte@users.noreply.github.com>
2024-01-23 23:41:39 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="a8512e83-39b5-4ac5-bde2-254f172430e7"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
